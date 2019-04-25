91076ad834007da05559ce083d20ee89f288e9fd
Kathryn <kathryn.chu10@gmail.com>
2019-04-25 13:05:12 -0700

    Tested api calls

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 // here, we will write the protocols to create our server
 
-var express = require('express'),
+const express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb', { useNewUrlParser: true }); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+// add express middleware which could be used to return 
+// more interactive messages. The snippet helps to redirect and respond 
+// whenever a wrong route is entered on the site.
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'});
+});
 
 app.listen(port);
 
